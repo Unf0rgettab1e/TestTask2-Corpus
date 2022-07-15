f590ff9d670fb4b9e8729338a134826571c1e034
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2022-07-15 17:35:31 +0300

    Initial Commit

---
+++ TestTask2-Corpus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask2-Corpus
+//
+//  Created by Tony on 15.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
